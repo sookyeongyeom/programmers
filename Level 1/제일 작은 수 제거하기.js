2d9f882095e1ef0bfaeff5a161e88e45df5c7ff7
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-04 10:06:15 +0900

    :sparkles: 220704

--- Level 1/제일 작은 수 제거하기.js
+++ Level 1/제일 작은 수 제거하기.js
@@ -1,14 +1,8 @@
 function solution(arr) {
-    // 오름차순 정렬
-    arr.sort((a, b) => {
-        return a > b ? 1 : -1;
-    });
-    const min = arr[0];
+    const min = Math.min(...arr);
+    const newArr = arr.filter((n) => n !== min);
 
-    // 제일 작은 수 제거
-    arr.pop(indexOf(min));
-
-    return arr.length ? arr : [-1];
+    return newArr.length ? newArr : [-1];
 }
 
 console.log(solution([4, 3, 2, 1]));
